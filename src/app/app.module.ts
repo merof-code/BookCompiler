88159e5cca6fd9873bb5cc6023e36d2e6dfba938
merof <misha.rosh@gmail.com>
2023-07-04 14:51:48 +0200

    finished moving to new dest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditorModule, TINYMCE_SCRIPT_SRC  } from '@tinymce/tinymce-angular';
-import { ProjectComponent } from './project/project.component';
+import { ProjectComponent } from './features/project/project.component';
 import { BookBrowserComponent } from './features/book-browser/book-browser.component';
 import { RichEditorComponent } from './features/rich-editor/rich-editor.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
